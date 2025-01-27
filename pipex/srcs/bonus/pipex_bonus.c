2a829f838eefcfdb9abee77c2932abbe0b727f07
Xxzer042xX <eljok87@gmail.com>
2025-01-27 17:11:45 +0100

    finish project see you next project

--- pipex/srcs/bonus/pipex_bonus.c
+++ pipex/srcs/bonus/pipex_bonus.c
@@ -19,30 +19,41 @@ static void	print_error_args(void);
 /*   Fonction principale du programme pipex bonus.                            */
 /*                                                                            */
 /*   Programme qui simule le comportement des pipes shell (|) et              */
-/*   redirections (<, >, <<, >>). Il permet d'exécuter plusieurs commandes    */
-/*   en chaîne en redirigeant leurs entrées/sorties.                          */
+/*   redirections (<, >, <<, >>). Permet d'exécuter une série de commandes    */
+/*   en redirigeant leurs entrées/sorties de manière chainée.                 */
 /*                                                                            */
 /*   Modes supportés :                                                        */
-/*   1. Mode standard : file1 cmd1 cmd2 ... cmdN file2                        */
-/*      Équivalent à : < file1 cmd1 | cmd2 | ... | cmdN > file2               */
-/*   2. Mode here_doc : here_doc LIMITER cmd1 cmd2 file2                      */
-/*      Équivalent à : cmd1 << LIMITER | cmd2 >> file2                        */
+/*   1. Mode standard : Multiples pipes                                       */
+/*      Format : ./pipex file1 cmd1 cmd2 ... cmdN file2                       */
+/*      Shell : < file1 cmd1 | cmd2 | ... | cmdN > file2                      */
+/*      Ex : ./pipex in "ls -l" "grep .c" "wc -l" out                         */
+/*                                                                            */
+/*   2. Mode here_doc : Entrée interactive                                    */
+/*      Format : ./pipex here_doc LIMITER cmd1 cmd2 file2                     */
+/*      Shell : cmd1 << LIMITER | cmd2 >> file2                               */
+/*      Ex : ./pipex here_doc EOF "grep hello" "wc -l" out                    */
 /*                                                                            */
 /*   Flux d'exécution :                                                       */
-/*   1. Vérification des arguments                                            */
-/*   2. Initialisation (init_app)                                             */
-/*   3. Création des processus (create_processes)                             */
-/*   4. Attente des processus (wait_processes)                                */
-/*   5. Nettoyage des ressources                                              */
+/*   1. Validation des arguments (minimum 4 + cas spécial here_doc)           */
+/*   2. Configuration initiale :                                              */
+/*      - Détection du mode (standard/here_doc)                               */
+/*      - Initialisation de la structure de données                           */
+/*   3. Préparation du pipeline :                                             */
+/*      - Initialisation des pipes et processus (init_app)                    */
+/*      - Configuration des fichiers d'entrée/sortie                          */
+/*   4. Exécution :                                                           */
+/*      - Création des processus enfants (create_processes)                   */
+/*      - Attente et collecte des statuts (wait_processes)                    */
+/*   5. Nettoyage complet des ressources (free_parent)                        */
 /*                                                                            */
 /*   Paramètres :                                                             */
-/*   - ac : nombre d'arguments                                                */
-/*   - av : tableau des arguments                                             */
-/*   - env : variables d'environnement                                        */
+/*   - ac : nombre d'arguments (min 5 en standard, 6 en here_doc)             */
+/*   - av : tableau des arguments du programme                                */
+/*   - env : variables d'environnement pour execve                            */
 /*                                                                            */
 /*   Retourne :                                                               */
-/*   - Le statut de sortie du dernier processus en cas de succès              */
-/*   - EXIT_FAILURE en cas d'erreur                                           */
+/*   - Statut de sortie du dernier processus si succès                        */
+/*   - EXIT_FAILURE si erreur d'arguments ou d'exécution                      */
 /*                                                                            */
 /* ************************************************************************** */
 int	main(int ac, char **av, char **env)
@@ -82,6 +93,7 @@ int	main(int ac, char **av, char **env)
 /*                                                                            */
 /*   Comportement :                                                           */
 /*   - Affiche les messages sur stderr (fd 2)                                 */
+/*   - Affiche le format d'utilisation correct                                */
 /*   - Termine le programme avec EXIT_FAILURE                                 */
 /*                                                                            */
 /* ************************************************************************** */
--- pipex/srcs/mandatory/pipex.c
+++ pipex/srcs/mandatory/pipex.c
@@ -16,33 +16,41 @@ static void	print_error_args(void);
 
 /* ************************************************************************** */
 /*                                                                            */
-/*   Fonction principale du programme pipex bonus.                            */
+/*   Fonction principale du programme pipex.                                  */
 /*                                                                            */
-/*   Programme qui simule le comportement des pipes shell (|) et              */
-/*   redirections (<, >, <<, >>). Il permet d'exécuter plusieurs commandes    */
-/*   en chaîne en redirigeant leurs entrées/sorties.                          */
+/*   Programme qui simule le comportement d'un pipe shell (|) et              */
+/*   des redirections (<, >). Il permet d'exécuter deux commandes             */
+/*   en séquence en redirigeant leurs entrées/sorties.                        */
 /*                                                                            */
-/*   Modes supportés :                                                        */
-/*   1. Mode standard : file1 cmd1 cmd2 ... cmdN file2                        */
-/*      Équivalent à : < file1 cmd1 | cmd2 | ... | cmdN > file2               */
-/*   2. Mode here_doc : here_doc LIMITER cmd1 cmd2 file2                      */
-/*      Équivalent à : cmd1 << LIMITER | cmd2 >> file2                        */
+/*   Format attendu :                                                         */
+/*   ./pipex file1 cmd1 cmd2 file2                                            */
+/*   Équivalent shell : < file1 cmd1 | cmd2 > file2                           */
+/*                                                                            */
+/*   Exemple :                                                                */
+/*   ./pipex infile "ls -l" "wc -l" outfile                                   */
+/*   Équivalent à : < infile ls -l | wc -l > outfile                          */
 /*                                                                            */
 /*   Flux d'exécution :                                                       */
-/*   1. Vérification des arguments                                            */
-/*   2. Initialisation (init_app)                                             */
-/*   3. Création des processus (create_processes)                             */
-/*   4. Attente des processus (wait_processes)                                */
-/*   5. Nettoyage des ressources                                              */
+/*   1. Vérification du nombre d'arguments (doit être 4)                      */
+/*   2. Initialisation de la structure de données                             */
+/*      - Allocation des pipes                                                */
+/*      - Ouverture des fichiers                                              */
+/*   3. Création de deux processus enfants                                    */
+/*      - Premier processus : exécute cmd1, lit infile, écrit dans pipe       */
+/*      - Second processus : exécute cmd2, lit du pipe, écrit dans outfile    */
+/*   4. Exécution :                                                           */
+/*      - Création des processus enfants (create_processes)                   */
+/*      - Attente et collecte des statuts (wait_processes)                    */
+/*   5. Nettoyage complet des ressources (free_parent)                        */
 /*                                                                            */
 /*   Paramètres :                                                             */
-/*   - ac : nombre d'arguments                                                */
-/*   - av : tableau des arguments                                             */
-/*   - env : variables d'environnement                                        */
+/*   - ac : nombre d'arguments (doit être 5)                                  */
+/*   - av : tableau des arguments [prog, file1, cmd1, cmd2, file2]            */
+/*   - env : variables d'environnement pour execve                            */
 /*                                                                            */
 /*   Retourne :                                                               */
 /*   - Le statut de sortie du dernier processus en cas de succès              */
-/*   - EXIT_FAILURE en cas d'erreur                                           */
+/*   - EXIT_FAILURE en cas d'erreur d'arguments ou d'exécution                */
 /*                                                                            */
 /* ************************************************************************** */
 int	main(int ac, char **av, char **env)
@@ -64,26 +72,23 @@ int	main(int ac, char **av, char **env)
 /*                                                                            */
 /*   Fonction d'affichage des erreurs d'arguments.                            */
 /*                                                                            */
-/*   Cette fonction affiche la syntaxe correcte du programme avec :           */
-/*   1. Mode standard :                                                       */
-/*      ./pipex_bonus input.txt "ls -l" "wc -l" output.txt                    */
-/*      Équivalent shell : < input.txt ls -l | wc -l > output.txt             */
+/*   Cette fonction affiche la syntaxe correcte du programme :                */
+/*   ./pipex infile cmd1 cmd2 outfile                                         */
+/*   Équivalent shell : < infile cmd1 | cmd2 > outfile                        */
 /*                                                                            */
-/*   2. Mode here_doc :                                                       */
-/*      ./pipex_bonus here_doc EOF "grep hello" "wc -l" output.txt            */
-/*      Équivalent shell : grep hello << EOF | wc -l >> output.txt            */
+/*   Exemple d'utilisation valide :                                           */
+/*   ./pipex input.txt "cat -e" "grep hello" output.txt                       */
+/*   Équivalent à : < input.txt cat -e | grep hello > output.txt              */
 /*                                                                            */
 /*   Comportement :                                                           */
-/*   - Affiche les messages sur stderr (fd 2)                                 */
+/*   - Affiche un message d'erreur sur stderr (fd 2)                          */
+/*   - Affiche le format d'utilisation correct                                */
 /*   - Termine le programme avec EXIT_FAILURE                                 */
 /*                                                                            */
 /* ************************************************************************** */
 static void	print_error_args(void)
 {
 	ft_putstr_fd("Error: Invalid number of arguments\n", 2);
-	ft_putstr_fd("Usage: ./bin/pipex_bonus", 2);
-	ft_putstr_fd(" <file1> <cmd1> <cmd2> <...> <file2>\n", 2);
-	ft_putstr_fd("   or: ./bin/pipex_bonus", 2);
-	ft_putstr_fd(" here_doc LIMITER <cmd1> <cmd2> <file2>\n", 2);
+	ft_putstr_fd("Usage: ./pipex infile cmd1 cmd2 outfile\n", 2);
 	exit(EXIT_FAILURE);
 }
